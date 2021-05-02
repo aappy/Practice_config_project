93a4dabe37bb4b36066f8cf8c005cef22353e977
aappy <aparnachdhry@gmail.com>
2021-05-02 12:03:52 -0700

    mvn proj

---
+++ src/main/java/Practice_config_project/Practice_config_project/App.java
@@ -0,0 +1,13 @@
+package Practice_config_project.Practice_config_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Practice_config_project/Practice_config_project/AppTest.java
@@ -0,0 +1,20 @@
+package Practice_config_project.Practice_config_project;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
